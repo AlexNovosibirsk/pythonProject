efec5e535da63d399c9acb82c76a7077122643f9
Alexander Gerasimov <alex.gerasimov@bk.ru>
2024-07-17 13:19:21 +0700

    Added  homework2.py

--- homework2.py
+++ homework2.py
@@ -6,5 +6,5 @@ print(f'Курс: {course_name}, всего задач: '
       f'{homework_completed_count}, '
       f' затрачено часов: {hours_spent_count}, \n'
       f'среднее время выполнения: {time_to_task} часа.')
-print('Курс: ',course_name, 'всего задач: ',homework_completed_count,
+print('Курс: ', course_name, 'всего задач: ',homework_completed_count,
       'затрачено часов: ', hours_spent_count, '\nсреднее время выполнения: ', time_to_task,' часа.')
