2a0f1a8f294efbb1c018279e5e1836aa60d014ba
Alexander Gerasimov <alex.gerasimov@bk.ru>
2024-07-17 14:38:05 +0700

    Added  homework3.py

--- homework3.py
+++ homework3.py
@@ -1,1 +1,16 @@
-print("kjfgkag")
+# идентификатор(переменная) name указывает на обьект класса str,
+# следовательно, к "name" применимы методы класса str, например:
+name = "Василий ИВАНОВx".title().strip("x")
+Name = "Tom" # это другая переменная, хотя звучит также
+print("Name:", name)
+age = 37
+print("Age: ", age)
+age += 5 #age = age + 5
+print("New Age: ", age)
+is_student = True
+print("Is Student: ", is_student)
+
+# при данном сочетании определений компилятор выдаст предупреждение
+age = 17
+age = 15 # Redeclared 'age' defined above without usage
+
