cfa5470689e44fb52c8b011136c451c9f5f6825f
Alexander Gerasimov <alex.gerasimov@bk.ru>
2024-07-17 12:46:38 +0700

    Added first homework1.py

---
+++ homework2.py
@@ -0,0 +1,10 @@
+homework_completed_count = 12
+hours_spent_count = 1.5
+course_name = 'Python'
+time_to_task = hours_spent_count / homework_completed_count
+print(f'Курс: {course_name}, всего задач: '
+      f'{homework_completed_count}, '
+      f' затрачено часов: {hours_spent_count}, \n'
+      f'среднее время выполнения: {time_to_task} часа.')
+print('Курс: ',course_name, 'всего задач: ',homework_completed_count,
+      'затрачено часов: ', hours_spent_count, '\nсреднее время выполнения: ', time_to_task,' часа.')
