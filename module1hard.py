9785d1cbcccf5f9bb5ec7dd928583b46e9cd9032
Alexander Gerasimov <alex.gerasimov@bk.ru>
2024-07-20 14:55:37 +0700

    Дополнительное практическое задание по модулю: "Базовые структуры данных."

--- module1hard.py
+++ module1hard.py
@@ -5,19 +5,14 @@ grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
 # преобразуем множество строк в список строк
-students = list(students)
-print(students)
-#students_list = list(students)
-# отсортируем список
-students.sort()
-print(students)
-#['Aaron', 'Bilbo', 'Johnny', 'Khendrik', 'Steve']
-par = {}
-
-# циклично расчитаем среднее и заполним коллекцию пар
+students = list(students)#students_list = list(students) не будем использовать дополнительную переменную
+students.sort()# отсортируем список
+print(students) #['Aaron', 'Bilbo', 'Johnny', 'Khendrik', 'Steve']
+pair = {}
+# циклично расчитаем среднее и заполним коллекцию pair ключ-значение
 for v in range(len(students)):
     average = sum(grades[v])/len(grades[v])
-    par.update({students[v]: average})
+    pair.update({students[v]: average})
 
-print(par)
+print(pair)
 #{'Aaron': 4.0, 'Bilbo': 2.25, 'Johhny': 4.0, 'Khendrik': 3.6666666666666665, 'Steve': 4.8}
