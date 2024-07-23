3c450d8c820d5060f88ac3a72adcfabeaa27851c
Alexander Gerasimov <alex.gerasimov@bk.ru>
2024-07-23 22:01:37 +0700

    "Всё не так уж просто"

--- module_2_4.py
+++ module_2_4.py
@@ -8,12 +8,12 @@
 
 primes = []
 not_primes = []
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+numbers = [1, 2, 3, -3, 4, 5, 6, 7, 8, -23, 0, 9, 10, 11, 12, 13, 14, 15]
 
 
 for i in range(len(numbers)):
 
-    if numbers[i] == 1:
+    if numbers[i] <= 1:
         continue
     is_prime = False
 
