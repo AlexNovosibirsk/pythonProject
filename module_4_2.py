5acd9a800b7472f991ff51281f36a3099734746d
AlexGerasimov <43001679+AlexNovosibirsk@users.noreply.github.com>
2024-08-07 22:36:51 +0700

    "Пространство имен."

--- module_4_2.py
+++ module_4_2.py
@@ -1,7 +1,8 @@
-
 """
 "Пространство имен."
 """
+
+
 # области видимости подразделяются на следующие:
 # локальная
 # объемлющая
@@ -11,12 +12,17 @@
 def test_function():
     # Объемлющая область видимости появляется тогда,
     # когда у нас одна функция находится внутри другой.
+
+    x = 6
+
     def inner_function():
+        nonlocal x # Х будет изменена во внутренней функции
+        x = 5
         print("Я в области видимости функции test_function")
+
     inner_function()
+    return x
 
-
-test_function() # Я в области видимости функции test_function
+print(test_function())  # Я в области видимости функции test_function
 #inner_function() #NameError: name 'inner_function' is not defined.
 # внутреннюю функцию снаружи запустить не удалось
-
