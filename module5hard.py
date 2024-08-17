c60d16da16c2e03fa3c874d822c20ce2189a3de3
Alexander Gerasimov <alex.gerasimov@bk.ru>
2024-08-17 21:17:19 +0700

    Задание "Свой YouTube"

--- module5hard.py
+++ module5hard.py
@@ -1,5 +1,11 @@
+import time
+
 
 class UrTube:
+    """
+    Класс видеоХостинг, содержащий объекты видео и объекты пользователей,
+    методы для работы с пользователями и с видеофайлами.
+    """
     def __init__(self):
         self.current_user = None # (текущий пользователь, User)
         self.current_password = 0
@@ -32,9 +38,16 @@ class UrTube:
     def __eq__(self, other):
         return self == other
 
+    def __str__(self):
+        return f"загружено {len(self.videos)} видео, зарегистрировано {len(self.users)} пользователей"
+
+    def __len__(self):
+        return len(self.videos)
+
     def add_videos(self, *args):
         for video_in in args:
             if isinstance(video_in, Video) is False:
+                print(f"объект  \"{video_in}\" не является объектом видео")
                 continue
             if video_in in self.videos:
                 print(f"файл \"{video_in.title}\" уже имеется")
@@ -58,10 +71,16 @@ class UrTube:
             return
         for video in self.videos:
             if name_video == video.title:
+                for video.time_now in range(1, video.duration + 1):
+                    time.sleep(0.1)
+                    print(video.time_now, end=" ")
                 print("Конец видео")
 
 
 class User:
+    """
+    “Класс пользователя, содержащий атрибуты: логин, пароль, возраст”
+    """
     def __init__(self, nickname="", password="", age=0):
         self.nickname = nickname  # имя пользователя
         self.password = hash(password)  # в хэшированном виде, число
@@ -69,21 +88,31 @@ class User:
 
 
 class Video:
+    """
+    “Класс видео, содержащий атрибуты: название, продолжительность, ограничение по возрасту”
+    """
     def __new__(cls, *args, **kwargs):
         return object.__new__(cls)
 
-    def __init__(self, title="", duration=0, time_now=0, adult_mode=False):
+    def __init__(self, title="", duration=0, adult_mode=False):
         self.title = title  # (заголовок, строка)
         self.duration = duration  # (продолжительность, секунды)
-        self.time_now = time_now  # (секунда остановки(изначально 0))
+        self.time_now = 0  # (секунда остановки(изначально 0))
         self.adult_mode = adult_mode  # (ограничение по возрасту)
 
+
+print(User.__doc__)
+print(Video.__doc__)
+print(UrTube.__doc__)
 
 ur = UrTube()
 v1 = Video('Лучший язык программирования 2024 года', 200)
 v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+v3 = Video('Физика для всех', 17)
 # Добавление видео
-ur.add_videos(v1, v2, v1, 7)
+ur.add_videos(v1, "abc", v2, v1, 7)
+ur.add_videos(v3)
+print(f"загружено {len(ur)} видео")
 
 # Проверка поиска
 print(ur.get_videos('лучший'))
@@ -101,4 +130,5 @@ ur.register('vasya_pupkin', 'lolkekcheburek', 13)
 ur.log_in('vasya_pupkin', 'lolkekcheburek')
 ur.watch_video('Для чего девушкам парень программист?')
 ur.log_in('vasya', 'F8098FM8fjm9jmi')
-ur.watch_video('Для чего девушкам парень программист?')
+ur.watch_video('Физика для всех')
+print(str(ur))
