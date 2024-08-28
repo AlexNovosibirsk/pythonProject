e89145d0b793188cd3d596ccb3c192cba8fea762
Alexander Gerasimov <alex.gerasimov@bk.ru>
2024-08-28 22:31:45 +0700

    Задание "Свой YouTube"

--- module5hard.py
+++ module5hard.py
@@ -1,68 +1,47 @@
 """
 Задание "Свой YouTube"
 """
-
 import time
 
-
 class UrTube:
-    """
-    Класс видеоХостинг, содержащий объекты видео и объекты пользователей,
-    методы для работы с пользователями и с видеофайлами.
-    """
-    current_user = None  # (текущий пользователь, User)
-    current_password = 0
-    current_age = 0
-    videos = []  # (список объектов Video)
-    users = []  # (список объектов User)
 
-    def search_users(self, nickname, password):
-        for user in self.users:
-            if nickname == user.nickname and hash(password) == user.password:
-                return user
-        return None
+    def __init__(self):
+        self.current_User_Object = None  # (текущий пользователь, User)
+        self.current_user = ""    # (текущий пользователь, NickName)
+        self.videos = []          # (список объектов Video)
+        self.users  = []          # (список объектов User)
 
     def log_in(self, nickname, password):
-        user = self.search_users(nickname, password)
-        if user is not None:
-            self.current_user = user.nickname
-            self.current_password = user.password
-            self.current_age = user.age
-            print(f"Пользователь {nickname} в аккаунте")
+        for user in self.users:
+            if nickname == user.nickname and hash(password) == user.password:
+                self.current_User_Object = user
+                self.current_user = user.nickname
+                break
 
     def register(self, nickname, password, age):
-        user = User(nickname, password, age)
-        if self.search_users(nickname, password) is not None:
-            print(f"Пользователь {nickname} уже существует")
-        else:
-            self.users.append(user)
-            self.current_user = user.nickname
-
-    def log_out(self):
-        self.current_user = None
-
-    def __eq__(self, other):
-        return self.current_user == other
-
-    def __str__(self):
-        return f"загружено {len(self.videos)} видео, зарегистрировано {len(self.users)} пользователей"
-
-    def __len__(self):
-        return len(self.videos)
-
-    def __contains__(self, item):
-        return item in self.videos
+        flag_search = False
+        for user in self.users:
+            if nickname == user.nickname:
+                print(f"Пользователь {nickname} уже существует")
+                flag_search = True
+                break
+        if not flag_search:
+            new_user = User(nickname, password, age)
+            self.users.append(new_user)
+            self.current_User_Object = new_user
+            self.current_user = nickname
 
     def add_videos(self, *args):
-        for video_in in args:
-            if isinstance(video_in, Video) is False:
-                print(f"объект  \"{video_in}\" не является объектом видео")
+        for new_video in args:
+            if isinstance(new_video, Video) is False:
                 continue
-            if self.__contains__(video_in): # if video_in in self.videos:
-                print(f"файл \"{video_in.title}\" уже имеется")
+            elif self.__contains__(new_video):
+                continue
             else:
-                self.videos.append(video_in)
-                print(f"файл \"{video_in.title}\" добавлен")
+                self.videos.append(new_video)
+
+    def __contains__(self, new_video):
+        return new_video in self.videos
 
     def get_videos(self, search_video=""):
         result = []
@@ -78,114 +57,73 @@ class UrTube:
         return None
 
     def watch_video(self, name_video):
-        if self.current_user is None:
+        if self.current_User_Object is None:
             print("Войдите в аккаунт, чтобы смотреть видео")
             return
-
         video = self.search_video(name_video)
-        if video is None:
-            return
-        elif video.adult_mode and self.current_age < 18:
-            print(f"{self.current_user}, Вам нет 18 лет, пожалуйста покиньте страницу")
-        else:
-            print(f"{self.current_user} смотрит \"{name_video}\",", end=" ")
-            for video.time_now in range(1, video.duration + 1):
-                time.sleep(0.1)
-                print(video.time_now, end=" ")
-            print("Конец видео")
+        if video:
+            if video.adult_mode and self.current_User_Object.age < 18:
+                print(f"{self.current_user}, Вам нет 18 лет, пожалуйста покиньте страницу")
+            else:
+                print(f"{self.current_user} смотрит \"{name_video}\",", end=" ")
+                for video.time_now in range(1, video.duration + 1):
+                    time.sleep(0.1)
+                    print(video.time_now, end=" ")
+                print("Конец видео")
+
+    def log_out(self):
+        self.current_user = None
 
 
 class User:
-    """
-    “Класс пользователя, содержащий атрибуты: логин, пароль, возраст”
-    """
-    nickname = ""  # имя пользователя
-    password = 0  # в хэшированном виде, число
-    age = 0  # возраст, число
-
-    def __init__(self, nickname="", password="", age=0):
-        self.nickname = nickname  # имя пользователя
+    def __init__(self, nickname, password, age):
+        self.nickname = nickname        # имя пользователя
         self.password = hash(password)  # в хэшированном виде, число
-        self.age = age  # возраст, число
+        self.age = age                  # возраст, число
 
 
 class Video:
-    """
-    “Класс видео, содержащий атрибуты: название, продолжительность, ограничение по возрасту”
-    """
-    title = ""  # (заголовок, строка)
-    duration = 0  # (продолжительность, секунды)
-    time_now = 0  # (секунда остановки(изначально 0))
-    adult_mode = False  # (ограничение по возрасту)
-
-    def __new__(cls, *args, **kwargs):
-        return object.__new__(cls)
 
     def __init__(self, title="", duration=0, adult_mode=False):
         self.title = title  # (заголовок, строка)
         self.duration = duration  # (продолжительность, секунды)
         self.adult_mode = adult_mode  # (ограничение по возрасту)
+        self.time_now = 0  # (секунда остановки(изначально 0))
 
+    def __str__(self):
+        return self.title
 
-print(User.__doc__)
-print(Video.__doc__)
-print(UrTube.__doc__)
+    def __repr__(self):
+        return self.title
+
 
 ur = UrTube()
+ur.register('urban_pythonist25', 'iScX4vIJClb9YQavjAgF', 25)
+ur.register('vasya22', 'F8098FM8fjm9jmi', 22)
+ur.register('petya12', 'lolkekcheburek', 12)
+ur.register('urban_pythonist25', 'iScX4vIJClb9YQavjAgF', 25)
+ur.register('anna17', 'dsfgj674jghm', 17)
+ur.register('vlad20', 'srtj123jtynz', 20)
+
+# Добавление видео
 v1 = Video('Лучший язык программирования 2024 года', 200)
 v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
 v3 = Video('Физика для всех', 17)
-# Добавление видео
+# print(repr(v1))
 ur.add_videos(v1, "abc", v2, v1, 7)
 ur.add_videos(v3)
-print(f"загружено {len(ur)} видео")
-
 # Проверка поиска
-# print(ur.get_videos('лучший'))
-# print(ur.get_videos('ПРОГ'))
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
 
-ur.watch_video('Физика для всех')
-ur.register('petya12', 'lolkekcheburek', 12)
-ur.register('urban_pythonist25', 'iScX4vIJClb9YQavjAgF', 25)
-ur.register('vasya22', 'F8098FM8fjm9jmi', 22)
-ur.register('vasya22', 'F8098FM8fjm9jmi', 22)
-ur.log_in('vasya22', 'F8098FM8fjm9jmi')
-# print(ur.current_user)
-ur.watch_video('Для чего девушкам парень программист?')
-ur.register('petya12', 'lolkekcheburek', 12)
+# Проверка входа в другой аккаунт
+ur.register('petya12', 'F8098FM8fjm9jmi', 55)
+
+ur.log_out()
+print(ur.current_user)
 ur.log_in('petya12', 'lolkekcheburek')
-ur.watch_video('Для чего девушкам парень программист?')
-ur.log_in('vasya22', 'F8098FM8fjm9jmi')
-# ur.log_out()
-ur.watch_video('Для чего девушкам парень программист?')
 ur.watch_video('Физика для всех')
-print(str(ur))
+ur.watch_video('Для чего девушкам парень программист?')
+print(ur.current_user)
 
-"""
-    “Класс пользователя, содержащий атрибуты: логин, пароль, возраст”
-
-    “Класс видео, содержащий атрибуты: название, продолжительность, ограничение по возрасту”
-    
-    Класс видеоХостинг, содержащий объекты видео и объекты пользователей,
-    методы для работы с пользователями и с видеофайлами.
-    
-файл "Лучший язык программирования 2024 года" добавлен
-объект  "abc" не является объектом видео
-файл "Для чего девушкам парень программист?" добавлен
-файл "Лучший язык программирования 2024 года" уже имеется
-объект  "7" не является объектом видео
-файл "Физика для всех" добавлен
-загружено 3 видео
-Войдите в аккаунт, чтобы смотреть видео
-Пользователь vasya22 уже существует
-Пользователь vasya22 в аккаунте
-vasya22 смотрит "Для чего девушкам парень программист?", 1 2 3 4 5 6 7 8 9 10 Конец видео
-Пользователь petya12 уже существует
-Пользователь petya12 в аккаунте
-petya12, Вам нет 18 лет, пожалуйста покиньте страницу
-Пользователь vasya22 в аккаунте
-vasya22 смотрит "Для чего девушкам парень программист?", 1 2 3 4 5 6 7 8 9 10 Конец видео
-vasya22 смотрит "Физика для всех", 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 Конец видео
-загружено 3 видео, зарегистрировано 3 пользователей
-"""
 
