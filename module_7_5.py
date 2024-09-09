685f5cf7173121ab8c4eac69c23f8686e637aa8c
Alexander Gerasimov <alex.gerasimov@bk.ru>
2024-09-09 21:36:15 +0700

    module_7_5

--- module_7_5.py
+++ module_7_5.py
@@ -3,15 +3,18 @@ import time
 
 
 if __name__ == "__main__":
+    # Небольшой эксперимент по созданию нового каталога
     try:
         os.mkdir("Second")
     except FileExistsError:
         print(f" Файл уже существует в {os.getcwd()}")
 
+    #  file_name = "Mother Goose - Monday’s Child.txt"
+    file_name = "module_7_5.py"
     directory = "."
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file == "Mother Goose - Monday’s Child.txt":
+            if file == file_name:
 
                 filepath = os.path.join(root, file)
                 filesize = os.path.getsize(file)
@@ -27,3 +30,8 @@ if __name__ == "__main__":
 # Обнаружен файл: Mother Goose - Monday’s Child.txt, Путь: .\Mother Goose - Monday’s Child.txt, Размер: 347 байт,
 # Время изменения: 30.06.2024 05:28,
 # Родительская директория: .
+
+# Обнаружен файл: module_7_5.py, Путь: .\module_7_5.py, Размер: 1371 байт,
+# Время изменения: 09.09.2024 21:35,
+# Родительская директория: .
+
