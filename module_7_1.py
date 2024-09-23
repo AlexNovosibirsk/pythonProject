a9f56c6b386b646b4025212c84637121018a1bbf
Alexander Gerasimov <alex.gerasimov@bk.ru>
2024-09-23 19:58:09 +0700

    Задача "Учёт товаров"

--- module_7_1.py
+++ module_7_1.py
@@ -40,14 +40,16 @@ class Shop:
                     file.write(str(pr) + "\n")
 
 
-
 def main():
     s1 = Shop()
     p1 = Product('Potato', 50.5, 'Vegetables')
     p2 = Product('Spaghetti', 3.4, 'Groceries')
     p3 = Product('Potato', 5.5, 'Vegetables')
+    p4 = Product('Banana', 7.5, 'Vegetables')
+    p5 = Product('Fish', 10.3, '  ')
+    p6 = Product('Meat', 9.8, '  ')
     print("p2->", p2)
-    s1.add(p1, p2, p3)
+    s1.add(p1, p2, p3, p4, p5, p6)
     print(s1.get_products())
 
 
