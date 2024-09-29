89c91d19c09deb0d167a8e58b92c6a82300b068b
Alexander Gerasimov <alex.gerasimov@bk.ru>
2024-09-29 21:55:32 +0700

    Задача "Многопроцессное считывание"

---
+++ module_10_5.py
@@ -0,0 +1,32 @@
+
+"""
+Задача "Многопроцессное считывание"
+"""
+import multiprocessing
+from datetime import datetime
+
+fileNames = [f'file {number}.txt' for number in range(1, 5)]
+
+def read_info(name):
+    all_data = list()
+    with open(name, "r") as file:
+        for line in file:
+            line = line.rstrip('\n')
+            all_data.append(line)
+    return name
+
+
+if __name__ == '__main__':
+
+# Линейный вызов
+#     start = datetime.now()
+#     print(list(map(read_info, fileNames)))
+#     end = datetime.now()
+#     print(end - start) # 0:00:15.142054
+# Многопроцессный
+    start = datetime.now()
+    with multiprocessing.Pool(processes=2) as pool:
+        pool.map(read_info, fileNames)
+    end = datetime.now()
+    print(end - start) # 0:00:10.845481
+
