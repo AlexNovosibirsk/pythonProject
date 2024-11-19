a44ddc2f2b048ffe303a91487f8f241bce8d2f92
AlexGerasimov <43001679+AlexNovosibirsk@users.noreply.github.com>
2024-11-19 19:48:50 +0700

    "Клавиатура кнопок"

--- module_13_5.py
+++ module_13_5.py
@@ -4,9 +4,10 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.dispatcher import FSMContext
 import asyncio
+from config import API
+import re
 
-api = "   "
-bot = Bot(token=api)
+bot = Bot(token=API)
 dp = Dispatcher(bot, storage=MemoryStorage())
 kb = ReplyKeyboardMarkup(resize_keyboard=True)
 button1 = KeyboardButton(text="Рассчитать")
@@ -14,11 +15,12 @@ button2 = KeyboardButton(text="Информация")
 kb.row(button1, button2)
 
 str_warning = "Задавайте только целые числа"
+tuple_data = dict()
 
 
-def calories_calculate(data):
-    calories_for_male = 10 * int(data['weight']) + 6.25 * int(data['growth']) + 5 * int(data['age']) + 5
-    calories_for_female = 10 * int(data['weight']) + 6.25 * int(data['growth']) + 5 * int(data['age']) - 161
+def calories_calculate(data: dict):
+    calories_for_male = 10 * data['weight'] + 6.25 * data['growth'] + 5 * data['age'] + 5
+    calories_for_female = 10 * data['weight'] + 6.25 * data['growth'] + 5 * data['age'] - 161
     return calories_for_male, calories_for_female
 
 
@@ -49,11 +51,13 @@ async def set_growth(message, state):
     await state.update_data(age=message.text)
     data = await state.get_data()
 
-    if not data['age'].isdigit():
-        await message.answer(str_warning)
-    else:
-        await message.answer(f"Ваш возраст: {data['age']}. Укажите свой рост:")
+    match = re.match(r'\d{1,3}', data['age'])
+    if match is not None:
+        tuple_data['age'] = int(match[0])
+        await message.answer(f"Ваш возраст: {match[0]}. Укажите свой рост:")
         await UserState.growth.set()
+    else:
+        await message.answer(str_warning)
 
 
 @dp.message_handler(state=UserState.growth)
@@ -61,11 +65,13 @@ async def set_weight(message, state):
     await state.update_data(growth=message.text)
     data = await state.get_data()
 
-    if not data['growth'].isdigit():
-        await message.answer(str_warning)
-    else:
-        await message.answer(f"Ваш рост: {data['growth']}, Укажите свой вес:")
+    match = re.match(r'\d{1,3}', data['growth'])
+    if match is not None:
+        tuple_data['growth'] = int(match[0])
+        await message.answer(f"Ваш рост: {match[0]}, Укажите свой вес:")
         await UserState.weight.set()
+    else:
+        await message.answer(str_warning)
 
 
 @dp.message_handler(state=UserState.weight)
@@ -73,14 +79,18 @@ async def send_calories(message, state):
     await state.update_data(weight=message.text)
     data = await state.get_data()
 
-    if not data['weight'].isdigit():
-        await message.answer(str_warning)
-    else:
-        calories_for_male, calories_for_female = calories_calculate(data)
+    match = re.match(r'\d{1,3}', data['weight'])
+    if match is not None:
+        await message.answer(f"Ваш вес: {match[0]}")
+        tuple_data['weight'] = int(match[0])
+        calories_for_male, calories_for_female = calories_calculate(tuple_data)
         await message.answer(f"Норма калории для мужчин: {calories_for_male}")
         await message.answer(f"Норма калории для женщин: {calories_for_female}")
         await state.finish()
+    else:
+        await message.answer(str_warning)
 
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
+
